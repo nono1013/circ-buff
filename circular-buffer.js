469cae04173fd0888d047b88c4f485e9758b9ef1
nono1013 <gnono1013@gmail.com>
2022-04-04 23:40:57 +0200

    Naive implementation

--- circular-buffer.js
+++ circular-buffer.js
@@ -4,37 +4,79 @@
 //
 
 class CircularBuffer {
-  constructor() {
-    throw new Error('Remove this statement and implement this function');
+  #arr
+  #size
+  #tail
+  #head
+  #next
+
+  constructor(size) {
+    this.#size = size
+    this.#arr = new Array(size)
+    this.#tail = 0
+    this.#head = 0
+    this.#next = 0
   }
 
-  write() {
-    throw new Error('Remove this statement and implement this function');
+  write(value) {
+    // TODO: handle 0 and 1 sizes
+
+    if (this.#next === this.#tail && this.#next !== this.#head) {
+      /// full and not first write
+      throw new BufferFullError()
+    }
+
+    /// set value
+    this.#arr[this.#next] = value
+
+    /// increase cursors
+    this.#head = this.#next
+    this.#next = (this.#next + 1) % this.#size
   }
 
   read() {
-    throw new Error('Remove this statement and implement this function');
+    if (this.#arr[this.#tail] === undefined) {
+      /// empty
+      throw new BufferEmptyError()
+    }
+    return this.#arr[this.#tail]
   }
 
-  forceWrite() {
-    throw new Error('Remove this statement and implement this function');
+  forceWrite(value) {
+    /// set value
+    this.#arr[this.#next] = value
+
+    /// increase cursors
+    if (this.#next === this.#tail) {
+      this.#tail = (this.#tail + 1) % this.#size
+    }
+    this.#head = this.#next
+    this.#next = (this.#next + 1) % this.#size
   }
 
   clear() {
-    throw new Error('Remove this statement and implement this function');
+    if (this.#head === this.#tail) {
+      /// empty, no more to clear
+      return
+    }
+
+    this.#arr[this.#tail] = undefined
+    this.#tail = (this.#tail + 1) % this.#size
   }
 }
 
-export default CircularBuffer;
+export default CircularBuffer
 
 export class BufferFullError extends Error {
   constructor() {
-    throw new Error('Remove this statement and implement this function');
+    super('Buffer is full')
+    this.name = 'BufferFullError'
   }
 }
 
 export class BufferEmptyError extends Error {
   constructor() {
-    throw new Error('Remove this statement and implement this function');
+    super('Buffer is empty')
+    this.name = 'BufferEmptyError'
   }
 }
--- circular-buffer.spec.js
+++ circular-buffer.spec.js
@@ -1,17 +1,31 @@
 import CircularBuffer, {
   BufferFullError,
   BufferEmptyError,
-} from './circular-buffer';
+} from './circular-buffer'
 
 describe('CircularBuffer', () => {
   test('reading empty buffer should fail', () => {
-    const buffer = new CircularBuffer(1);
-    expect(() => buffer.read()).toThrow(BufferEmptyError);
-  });
+    const buffer = new CircularBuffer(1)
+    expect(() => buffer.read()).toThrow(BufferEmptyError)
+  })
 
   test('can read an item just written', () => {
-    const buffer = new CircularBuffer(1);
-    buffer.write('1');
-    expect(buffer.read()).toBe('1');
-  });
-});
+    const buffer = new CircularBuffer(1)
+    buffer.write('1')
+    expect(buffer.read()).toBe('1')
+  })
+
+  test('can read items', () => {
+    const buffer = new CircularBuffer(5)
+    buffer.write('1')
+    buffer.write('2')
+    buffer.write('3')
+    expect(buffer.read()).toBe('1')
+    buffer.clear()
+    expect(buffer.read()).toBe('2')
+    buffer.write('4')
+    buffer.write('5')
+    buffer.write('6')
+    expect(() => buffer.write('7')).toThrow(BufferFullError)
+  })
+})
